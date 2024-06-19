23904f62b4d148788bb77ab573b13e75182be789
rakawanegan <ren.nakagawa.r@gmail.com>
2024-06-19 19:07:25 +0900

    fix answer

--- src/main.py
+++ src/main.py
@@ -67,7 +67,7 @@ class RailwayKnowledgeSystemWithRinnaGPT2:
             pad_token_id=self.tokenizer.pad_token_id,
             no_repeat_ngram_size=2,
         )
-        answer = self.tokenizer.decode(outputs[0], skip_special_tokens=True)[len(prompt):]
+        answer = ''.join(self.tokenizer.decode(outputs[0], skip_special_tokens=True).split('answer:')[1:])
         return answer
 
     def inference(self, query: str) -> str:
