bf21e7631ac7c3830ad537613d750ad01b672528
Ren Nakagawa <rennakagawa@RennoMacBook-Pro.local>
2024-11-04 17:34:28 +0900

    add incident knowledge

--- src/main.py
+++ src/main.py
@@ -1,6 +1,8 @@
 import os
 from glob import glob
+import re
 import numpy as np
+import pandas as pd
 import markdown
 from bs4 import BeautifulSoup
 import torch
@@ -18,14 +20,14 @@ class RailwayKnowledgeSystemWithRinnaGPT2:
         self.sentences = knowledge.split('\n')
         self.tokenizer = AutoTokenizer.from_pretrained("rinna/japanese-gpt2-medium")
         self.model = GPT2LMHeadModel.from_pretrained("rinna/japanese-gpt2-medium")
-        if os.path.exists("./cache/index.faiss"):
-            self.index = faiss.read_index("./cache/index.faiss")
+        if os.path.exists("./cache/rinna-railway-knowledge-index.faiss"):
+            self.index = faiss.read_index("./cache/rinna-railway-knowledge-index.faiss")
         else:
             embeddings = self._embed_text(self.sentences)
             self.index = faiss.IndexFlatL2(embeddings.shape[1])
             self.index.add(embeddings)
             os.makedirs("./cache", exist_ok=True)
-            faiss.write_index(self.index, "./cache/index.faiss")
+            faiss.write_index(self.index, "./cache/rinna-railway-knowledge-index.faiss")
 
     def _load_markdown_file(self, file_path: str) -> str:
         with open(file_path, 'r', encoding='utf-8') as f:
@@ -105,14 +107,14 @@ class MakeRailwayKnowledgePromptWithTohokuBERT:
         self.sentences = knowledge.split('\n')
         self.tokenizer = AutoTokenizer.from_pretrained("cl-tohoku/bert-base-japanese")
         self.model = AutoModel.from_pretrained("cl-tohoku/bert-base-japanese")
-        if os.path.exists("./cache/index.faiss"):
-            self.index = faiss.read_index("./cache/index.faiss")
+        if os.path.exists("./cache/railway-knowledge-index.faiss"):
+            self.index = faiss.read_index("./cache/railway-knowledge-index.faiss")
         else:
             embeddings = self._embed_text(self.sentences)
             self.index = faiss.IndexFlatL2(embeddings.shape[1])
             self.index.add(embeddings)
             os.makedirs("./cache", exist_ok=True)
-            faiss.write_index(self.index, "./cache/index.faiss")
+            faiss.write_index(self.index, "./cache/railway-knowledge-index.faiss")
 
     def _load_markdown_file(self, file_path: str) -> str:
         with open(file_path, 'r', encoding='utf-8') as f:
@@ -138,3 +140,61 @@ class MakeRailwayKnowledgePromptWithTohokuBERT:
         basis = self.get_basis(query)
         prompt = f"Context: {basis}\nQuery: {query}\nAnswer:"
         return prompt
+
+class MakeRailwayIncidentCasePromptWithTohokuBERT:
+    def __init__(self, k: int) -> None:
+        self.k = k
+        knowledge = ''
+        p_file = './data/RailwayIncidentCase.csv'
+        self.sentences += self._load_csv_file(p_file)
+        self.tokenizer = AutoTokenizer.from_pretrained("cl-tohoku/bert-base-japanese")
+        self.model = AutoModel.from_pretrained("cl-tohoku/bert-base-japanese")
+        if os.path.exists("./cache/railway-incident-index.faiss"):
+            self.index = faiss.read_index("./cache/railway-incident-index.faiss")
+        else:
+            embeddings = self._embed_text(self.sentences)
+            self.index = faiss.IndexFlatL2(embeddings.shape[1])
+            self.index.add(embeddings)
+            os.makedirs("./cache", exist_ok=True)
+            faiss.write_index(self.index, "./cache/railway-incident-index.faiss")
+
+    def _load_csv_file(self, file_path: str) -> list[str]:
+        df = pd.read_csv(file_path, sep='\t', encoding='utf-16')
+        texts = list()
+        for i, row in df.iterrows():
+            row = str(dict(row))
+            row = re.sub(r'\{|\}', '', row)  # strip {}
+            row = re.sub(r"'", '"', row)  # replace ' with "
+            texts.append(row)
+        return texts
+
+    def _embed_text(self, texts: list[str]) -> np.ndarray:
+        inputs = self.tokenizer(texts, padding=True, truncation=True, return_tensors="pt")
+        with torch.no_grad():
+            embeddings = self.model(**inputs).last_hidden_state.mean(dim=1)
+        return embeddings.numpy()
+
+    def get_text(self) -> str:
+        return '\n'.join(self.sentences)
+
+    def get_basis(self, query: str) -> None:
+        query_embedding = self._embed_text([query])
+        _, indices = self.index.search(query_embedding, self.k) # first output is distance
+        basis = " ".join([self.sentences[i] for i in indices[0] if i < len(self.sentences)])
+        return basis
+
+    def make_prompt(self, query: str) -> None:
+        basis = self.get_basis(query)
+        prompt = f"Context: {basis}\nQuery: {query}\nAnswer:"
+        return prompt
+    
+
+def set_up():
+    RailwayKnowledgeSystemWithRinnaGPT2(k=1)
+    MakeRailwayIncidentCasePromptWithTohokuBERT(k=1)
+    MakeRailwayKnowledgePromptWithTohokuBERT(k=1)
+
+
+if __name__ == '__main__':
+    set_up()
+    print("Setup done.")
